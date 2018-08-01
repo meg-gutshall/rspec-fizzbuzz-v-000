c7cb3424939d8165351f280367960eaa361b3867
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-08-01 19:02:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,11 @@
 
 def fizzbuzz(int)
   if int % 15 == 0
+# Or if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   elsif int % 5 == 0
     "Buzz"
   elsif int % 3 == 0
     "Fizz"
-  else
-    nil
   end
 end
